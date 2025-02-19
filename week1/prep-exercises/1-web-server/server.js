ebfe47448ab812a74590af6a5b5619ab9eb95861
Volodymyr-coder <volodymyr.coder2024@gmail.com>
2025-02-19 10:17:22 +0100

    lost prepexer

--- week1/prep-exercises/1-web-server/server.js
+++ week1/prep-exercises/1-web-server/server.js
@@ -2,13 +2,48 @@
  * Exercise 3: Create an HTTP web server
  */
 
+const fs = require('fs');
+const url = require('url');
+const path = require('path');
+
 const http = require('http');
 
 //create a server
 let server = http.createServer(function (req, res) {
-  // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
+  if (req.url === '/') {
+    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Error loading file ');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else if (req.url === '/index.js') {
+    fs.readFile(path.join(__dirname, 'index.js'), (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end(' Error loading file');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'application/javascript' });
+        res.end(data);
+      }
+    });
+  } else if (req.url === '/style.css') {
+    fs.readFile(path.join(__dirname, 'style.css'), (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Error loading file');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+        res.end(data);
+      }
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Page Not Found');
+  }
 });
 
 server.listen(3000); // The server starts to listen on port 3000
